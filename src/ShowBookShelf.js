c280f784312325ea8c4f9d2f6a6ed05e2c4adf72
Foteini Kollia <Foteinikollia@gmail.com>
2018-07-18 13:22:56 +0300

    fix:remove unneccessary component

--- src/ShowBookShelf.js
+++ src/ShowBookShelf.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import BookLook from "./BookLook";
+// import BookLook from "./BookLook";
 import * as BooksAPI from "./BooksAPI";
 
 class ShowBookShelf extends Component {
