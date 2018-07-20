8a83356c72aa8913df57047cc2a8f3ac47a579ed
Foteini Kollia <Foteinikollia@gmail.com>
2018-07-20 21:18:52 +0300

    feat:add BookLook component file

---
+++ src/BookLook.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import noImgAvailable from "./icons/no_image_available.svg.png";
+
+class BookLook extends Component {
+
+  render() {
+    const { book } = this.props;
+
+    return (
+      <li key={book.id}>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover">
+              {(book.imageLinks && (
+                <img src={book.imageLinks.smallThumbnail} alt={book.title} />
+              )) || <img src={noImgAvailable} alt={book.title} />}
+            </div>
+
+            <div className="book-shelf-changer">
+              <select
+                value={book.shelf}
+                onChange={event =>
+                  this.props.changeShelf(book, event.target.value)
+                }
+              >
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead"> Want to Read</option>
+                <option value="read"> Read</option>
+                <option value="none"> None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
+        </div>
+      </li>
+    );
+  }
+}
+export default BookLook;
