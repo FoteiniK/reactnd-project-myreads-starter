e572e44333bfda4f815fb9eb3981641c7bbbf592
Foteini Kollia <Foteinikollia@gmail.com>
2018-07-22 15:10:29 +0300

    feat:Add file for NoMatch component

---
+++ src/NoMatch.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class NoMatch extends Component {
+  render() {
+    return (
+      <div className="No-Match">
+        <h1>Oops!</h1>
+        <h2>404 Not Found</h2>
+        <p>Sorry, an error has occured, Requested page not found!</p>
+        <div>
+          <Link to="/">Return to main page</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NoMatch;
